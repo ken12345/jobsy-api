eb3a1201b213a505b4a17ab9d195edbe5f8fc765
Ariel Jericko Gacilo <42305618+bokzgacilo@users.noreply.github.com>
2026-01-28 21:11:26 +0800

    Add remote server URL to Swagger configuration

--- src/swagger.ts
+++ src/swagger.ts
@@ -13,6 +13,10 @@ const options: swaggerJsdoc.Options = {
         url: 'http://localhost:3000/api',
         description: 'Jobsy Dev Server',
       },
+      {
+        url: 'http://72.61.119.52:5000/api',
+        description: 'Jobsy Dev Remote Server',
+      },
     ],
   },
   apis: ['./src/routes/*.ts', './src/controllers/*.ts'],

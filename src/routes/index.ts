f4d9331945fa3cc55a1e352a2793c9541653feb8
ken12345 <kengacilo@gmail.com>
2026-02-06 06:44:56 +0800

    remove unnecesary

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 
 import express, { Router } from 'express';
 import userRoutes from './user.routes';
-import uploadRoutes from './bucket-uploader.routes';
 import merchantRoutes from './merchant.routes';
 import { validateApiKey } from '../middleware/auth';
 import productRoutes from './products.routes';
@@ -9,7 +8,6 @@ const apiRouter: Router = express.Router();
 
 apiRouter.use('/users', validateApiKey, userRoutes);
 apiRouter.use("/merchants", validateApiKey, merchantRoutes)
-apiRouter.use("/upload", uploadRoutes);
 apiRouter.use("/merchants", validateApiKey, merchantRoutes);
 apiRouter.use("/products", validateApiKey, productRoutes);
 
